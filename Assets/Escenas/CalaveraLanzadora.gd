867259310408455c41937b287de633195abf3a79
Krodha <lucascrucesfernandez@gmail.com>
2022-08-02 22:03:02 +0200

    Calaverda Lanzacosas
    La calaveralanzacosas ya lanza cosas, pero no lo hace del todo bien, hay algún fallo con las colisiones o algo

--- Assets/Escenas/CalaveraLanzadora.gd
+++ Assets/Escenas/CalaveraLanzadora.gd
@@ -6,6 +6,8 @@ var can_move : bool = false
 export (PackedScene) var balaEnemy
 onready var calavera = get_tree().get_nodes_in_group("Enemy")[3]
 onready var jugador = get_tree().get_nodes_in_group("Player")[0]
+onready var rng : RandomNumberGenerator = RandomNumberGenerator.new()
+onready var bala_velocidad = 2000
 
 func _process(delta):
 	position += movimiento * delta
@@ -31,6 +33,8 @@ func movimiento_ctrl():
 	
 	if vidas_calavera <= 0:
 		queue_free()
+	
+	$Position2D.look_at(jugador.global_position)
 
 func _on_HitBox_body_entered(body):
 	if body.is_in_group("Proyectil"):
@@ -45,4 +49,18 @@ func _on_Area2D_body_entered(body):
 func disparo():
 	var bala = balaEnemy.instance()
 	bala.position = global_position
-	bala.apply_impulse()
+	bala.rotation_degrees = $Position2D.rotation_degrees
+	bala.apply_impulse(Vector2(),Vector2(bala_velocidad,0).rotated($Position2D.rotation))
+	get_tree().get_root().call_deferred("add_child", bala)
+
+
+func random(min_val,max_val):
+	rng.randomize()
+	var random = rng.randf_range(min_val,max_val)
+	return random
+
+
+func _on_Disparo_timeout():
+	disparo()
+	$Disparo.wait_time = random(2.5,5)
+	$Disparo.start()
